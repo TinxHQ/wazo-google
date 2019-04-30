f3d45742ad8486f4121fb0c0b2322ae81eaca19b
Pascal Cadotte Michaud <pcm@wazo.io>
2019-04-30 08:23:01 -0400

    dird: move the source http interface to its own module

--- integration_tests/suite/test_dird_google.py
+++ integration_tests/suite/test_dird_google.py
@@ -9,13 +9,11 @@ from hamcrest import (
     calling,
     contains,
     empty,
-    equal_to,
     has_entries,
     has_item,
     has_properties,
     has_property,
     is_,
-    not_,
 )
 from xivo_test_helpers.auth import AuthClient as AuthMock
 from xivo_test_helpers.hamcrest.raises import raises
@@ -129,72 +127,6 @@ class TestDirdClientGooglePlugin(BaseGoogleTestCase):
             pass
 
         super().tearDown()
-
-    def test_when_create_source_then_no_error(self):
-        assert_that(
-            calling(self.client.backends.create_source).with_args(
-                backend=self.BACKEND,
-                body=self.config(),
-            ),
-            not_(raises(requests.HTTPError))
-        )
-
-    def test_given_source_when_delete_then_ok(self):
-        source = self.client.backends.create_source(backend=self.BACKEND, body=self.config())
-
-        assert_that(
-            calling(self.client.backends.delete_source).with_args(
-                backend=self.BACKEND,
-                source_uuid=source['uuid'],
-            ),
-            not_(raises(requests.HTTPError))
-        )
-
-    def test_when_delete_then_raises(self):
-        assert_that(
-            calling(self.client.backends.delete_source).with_args(
-                backend=self.BACKEND,
-                source_uuid='a-non-existing-source-uuid',
-            ),
-            raises(requests.HTTPError).matching(
-                has_property('response', has_properties('status_code', 404))
-            )
-        )
-
-    def test_given_source_when_get_then_ok(self):
-        config = self.config()
-
-        created = self.client.backends.create_source(backend=self.BACKEND, body=config)
-
-        source = self.client.backends.get_source(backend=self.BACKEND, source_uuid=created['uuid'])
-        assert_that(source, equal_to(created))
-
-    def test_given_source_when_edit_then_ok(self):
-        source = self.client.backends.create_source(backend=self.BACKEND, body=self.config())
-        source.update({'name': 'a-new-name'})
-
-        assert_that(
-            calling(self.client.backends.edit_source).with_args(
-                backend=self.BACKEND,
-                source_uuid=source['uuid'],
-                body=source,
-            ),
-            not_(raises(requests.HTTPError))
-        )
-
-        updated = self.client.backends.get_source(backend=self.BACKEND, source_uuid=source['uuid'])
-        assert_that(updated, has_entries(
-            uuid=source['uuid'],
-            name='a-new-name',
-        ))
-
-    def test_given_source_when_list_sources_then_ok(self):
-        source = self.client.backends.create_source(backend=self.BACKEND, body=self.config())
-
-        sources = self.client.backends.list_sources(backend=self.BACKEND)
-        assert_that(sources, has_entries(
-            items=contains(source),
-        ))
 
     @pytest.mark.skip(reason='Not implemented')
     def test_given_source_and_google_when_list_contacts_then_contacts_listed(self):
